a1ffde7ca0f665455eec541d0926da780dab2b91
Igor Siamykin <sparkisisss@gmail.com>
2025-06-19 16:40:24 +0300

    ECOM-22: Add burger-menu in header

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,37 +1,46 @@
 import classes from './Header.module.css'
 import { Link } from 'react-router'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [isActive, setIsActive] = useState<boolean>(false)
+
   return (
     <header className={classes.header}>
-      <div className="container">
-        <div className={classes.header__row}>
-          <div className={classes.header__logo}>
-            <img src="/icons/logo.svg" alt="Logo" />
-            <span>Fashion</span>
-          </div>
-          <nav className={classes.header__nav}>
-            <ul>
-              <li>
-                <Link to="/catalogue">CATALOGUE</Link>
-              </li>
-              <li>
-                <Link to="/">FASHION</Link>
-              </li>
-              <li>
-                <Link to="/basket">BASKET</Link>
-              </li>
-              <li>
-                <Link to="/selected">SELECTED</Link>
-              </li>
-              <li>
-                <Link to="/login" className={classes.header__nav_btn}>
-                  SIGN UP
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className={classes.header__logo}>
+        <img src="/icons/logo.svg" alt="Logo" />
+        <span>Fashion</span>
+      </div>
+
+      <nav
+        className={`${classes.header__nav} ${isActive ? classes.header__navActive : ''}`}
+      >
+        <ul>
+          <li>
+            <Link to="/catalogue">CATALOGUE</Link>
+          </li>
+          <li>
+            <Link to="/">FASHION</Link>
+          </li>
+          <li>
+            <Link to="/basket">BASKET</Link>
+          </li>
+          <li>
+            <Link to="/selected">SELECTED</Link>
+          </li>
+          <li>
+            <Link to="/login" className={classes.header__nav_btn}>
+              SIGN UP
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div
+        className={`${classes.burger} ${isActive ? classes.burgerActive : ''}`}
+        onClick={() => setIsActive(!isActive)}
+      >
+        <span></span>
       </div>
     </header>
   )
