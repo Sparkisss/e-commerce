9ad0fcf22c239f7286eb32d28805a01cb6c3e686
Igor Siamykin <sparkisisss@gmail.com>
2025-06-19 18:38:37 +0300

    ECOM-23: Add responsive layout - main page

--- src/widgets/brands/ui/Brands.tsx
+++ src/widgets/brands/ui/Brands.tsx
@@ -3,40 +3,26 @@ import classes from './Brands.module.css'
 export const Brands = () => {
   return (
     <section className={classes.brands}>
-      <div className="container">
-        <ul className={classes.brands__list}>
-          <li>
-            <a href="#!">
-              <img src="/brands/HM.png" alt="hm" />
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <img src="/brands/Obey.png" alt="obey" />
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <img src="/brands/Levis.png" alt="levis" />
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <img src="/brands/Amazon.png" alt="amazon" />
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <img src="/brands/Lacoste.png" alt="lacoste" />
-            </a>
-          </li>
-          <li>
-            <a href="#!">
-              <img src="/brands/Shopify.png" alt="shopify" />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className={classes.brands__list}>
+        <li>
+          <img src="/brands/HM.png" alt="hm" />
+        </li>
+        <li>
+          <img src="/brands/Obey.png" alt="obey" />
+        </li>
+        <li>
+          <img src="/brands/Levis.png" alt="levis" />
+        </li>
+        <li>
+          <img src="/brands/Amazon.png" alt="amazon" />
+        </li>
+        <li>
+          <img src="/brands/Lacoste.png" alt="lacoste" />
+        </li>
+        <li>
+          <img src="/brands/Shopify.png" alt="shopify" />
+        </li>
+      </ul>
     </section>
   )
 }
--- src/widgets/promo/ui/Promo.tsx
+++ src/widgets/promo/ui/Promo.tsx
@@ -5,37 +5,33 @@ import classNames from 'classnames'
 export const Promo = () => {
   return (
     <section className={classes.promo}>
-      <div className="container">
-        <div className={classes.promo__content}>
-          <div>
-            <div className={classes.promo__title}>
-              <span className={classes.highlight}>
-                <span>LET’S</span>
-              </span>
-              EXPLORE
-              <span
-                className={classNames(
-                  classes.highlight,
-                  classes['highlight__yellow']
-                )}
-              >
-                <span> UNIQUE</span>
-              </span>
-              CLOTHES.
-            </div>
-            <div className={classes.promo__desc}>
-              Live for Influential and Innovative fashion!
-            </div>
-            <div className={classes.promo__btn_wrapper}>
-              <Link to="/catalogue" className={classes.promo__btn}>
-                Shop Now
-              </Link>
-            </div>
-          </div>
-          <div>
-            <img src="/images/header-img.jpg" alt="Promo" />
-          </div>
+      <div className={classes.promo__content}>
+        <div className={classes.promo__title}>
+          <span className={classes.highlight}>
+            <span>LET’S</span>
+          </span>
+          EXPLORE
+          <span
+            className={classNames(
+              classes.highlight,
+              classes['highlight__yellow']
+            )}
+          >
+            <span> UNIQUE</span>
+          </span>
+          CLOTHES.
         </div>
+        <div className={classes.promo__desc}>
+          Live for Influential and Innovative fashion!
+        </div>
+        <div className={classes.promo__btn_wrapper}>
+          <Link to="/catalogue" className={classes.promo__btn}>
+            Shop Now
+          </Link>
+        </div>
+      </div>
+      <div className={classes.promo__img}>
+        <img src="/images/header-img.jpg" alt="Promo" />
       </div>
     </section>
   )
